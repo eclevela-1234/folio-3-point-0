28732b2c531308f3c9718e1717522c992a7c370b
Eliot Cleveland <eclevela@richmond.edu>
2022-10-12 22:43:37 -0400

    added some styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import coverImage from "../../assets/cover/cover-image.jpg";
+import profileImage from "../../assets/cover/profileimage.jpg";
 
 function About() {
 
   return (
-    <section className="my-5" >
-      <h1 id="about">Meet Eliot</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+    <section className="my-2" >
+      
+      <img src={profileImage} id="profileImg" className="" style={{ width: "100%" }} alt="profile" />
+      <h1 classname="my-2" id="about">Meet the Developer</h1>
       <div className="my-2">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
