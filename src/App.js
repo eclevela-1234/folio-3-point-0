7787c26b340b2578ccb3218fb2fd229ab67f1194
Eliot Cleveland <eclevela@richmond.edu>
2022-11-07 21:46:38 -0500

    messing with routes

--- src/App.js
+++ src/App.js
@@ -6,52 +6,69 @@ import ContactForm from "./components/Contact";
 import Resume from "./components/Resume";
 import Footer from "./components/Footer";
 import { capitalizeFirstLetter } from "./utils/helpers";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [categories] = useState([
-    { name: "about Me", description: ""},
-    { name: "portfolio",
-      description:
-        ""},
-    { name: "resume", description: ""}
 
-  ]);
-  const [contactSelected, setContactSelected] = useState(false);
-
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  const renderPage = () => {
-    console.log("currentCat", currentCategory);
-    switch (currentCategory.name) {
-      case "about Me":
-        return <About currentCategory={currentCategory} />;
-      case "portfolio":
-        return <Portfolio currentCategory={currentCategory}/>;
-      case "resume":
-        return <Resume currentCategory={currentCategory}/>;
-      default:
-        return <About currentCategory={currentCategory}/>;
-    }
-  };
   return (
-    <div>
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      />
-      <main>
-        {!contactSelected ? (
-          <>
-      <div>{renderPage(currentCategory)}</div>
-        </>
-        ) : (<ContactForm/>)}
-      </main>
-      <Footer />
-    </div>
-  );
+    <Router>
+      <div className="flex-column justify-center min-100-vh">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/" element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  )
+  // const [categories] = useState([
+  //   { name: "about Me", description: ""},
+  //   { name: "portfolio",
+  //     description:
+  //       ""},
+  //   { name: "resume", description: ""}
+
+  // ]);
+  // const [contactSelected, setContactSelected] = useState(false);
+
+  // const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  // const renderPage = () => {
+  //   console.log("currentCat", currentCategory);
+  //   switch (currentCategory.name) {
+  //     case "about Me":
+  //       return <About currentCategory={currentCategory} />;
+  //     case "portfolio":
+  //       return <Portfolio currentCategory={currentCategory}/>;
+  //     case "resume":
+  //       return <Resume currentCategory={currentCategory}/>;
+  //     default:
+  //       return <About currentCategory={currentCategory}/>;
+  //   }
+  // };
+  // return (
+  //   <div>
+  //     <Nav
+  //       categories={categories}
+  //       setCurrentCategory={setCurrentCategory}
+  //       currentCategory={currentCategory}
+  //       contactSelected={contactSelected}
+  //       setContactSelected={setContactSelected}
+  //     />
+  //     <main>
+  //       {!contactSelected ? (
+  //         <>
+  //     <div>{renderPage(currentCategory)}</div>
+  //       </>
+  //       ) : (<ContactForm/>)}
+  //     </main>
+  //     <Footer />
+  //   </div>
+  // );
+
+
 }
 
 export default App;
