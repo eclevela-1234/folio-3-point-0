25c4f45eea8d906c3d43f5714f387dfec14dd3a3
Eliot Cleveland <eclevela@richmond.edu>
2022-11-08 23:13:58 -0500

    new file structure, starting over with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,80 +1,18 @@
-import React, { useState } from "react";
-import Tabs from "./components/Nav";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import ContactForm from "./components/Contact";
-import Resume from "./components/Resume";
+
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { capitalizeFirstLetter } from "./utils/helpers";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 function App() {
-
   return (
-    <Router>
-      <div className="min-100-vh">
-        
-        <div className="container">
-
-          <Tabs />
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/contact" element={<ContactForm />} />
-          </Routes>
-          <Footer/>
-        </div>
-        
-      </div>
-    </Router>
-  )
-  // const [categories] = useState([
-  //   { name: "about Me", description: ""},
-  //   { name: "portfolio",
-  //     description:
-  //       ""},
-  //   { name: "resume", description: ""}
-
-  // ]);
-  // const [contactSelected, setContactSelected] = useState(false);
-
-  // const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  // const renderPage = () => {
-  //   console.log("currentCat", currentCategory);
-  //   switch (currentCategory.name) {
-  //     case "about Me":
-  //       return <About currentCategory={currentCategory} />;
-  //     case "portfolio":
-  //       return <Portfolio currentCategory={currentCategory}/>;
-  //     case "resume":
-  //       return <Resume currentCategory={currentCategory}/>;
-  //     default:
-  //       return <About currentCategory={currentCategory}/>;
-  //   }
-  // };
-  // return (
-  //   <div>
-  //     <Nav
-  //       categories={categories}
-  //       setCurrentCategory={setCurrentCategory}
-  //       currentCategory={currentCategory}
-  //       contactSelected={contactSelected}
-  //       setContactSelected={setContactSelected}
-  //     />
-  //     <main>
-  //       {!contactSelected ? (
-  //         <>
-  //     <div>{renderPage(currentCategory)}</div>
-  //       </>
-  //       ) : (<ContactForm/>)}
-  //     </main>
-  //     <Footer />
-  //   </div>
-  // );
-
-
+    <div className="App">
+      <Container>
+        {/* <Header /> */}
+        <Footer />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 function Footer() {
   return (
     <footer>
-      <div className=" footer flex-row my-2">
+      <Row>
+        <Col>
+        <div id="social">
         <div className="mx-1">
           <a
             href="https://www.linkedin.com/in/eliotcleveland/"
@@ -21,8 +26,8 @@ function Footer() {
           <a href="https://www.facebook.com/eliot.cleveland/" target={`_blank`}>
             <i className="fa-brands fa-facebook-f"></i>
           </a>
-        </div>
-      </div>
+        </div></div></Col>
+      </Row>
     
       <h6 id="copy"><i className="fa-regular fa-copyright"></i>2022 Eliot CLeveland</h6>
     </footer>
